ac88432bf647b4f0b663645453885b1e0bf2fcd0
Francesco <capponi.francesco@gmail.com>
2014-03-11 11:19:41 +0100

    Sincronizzazione

--- MilanoBlu2/src/com/cesena/milanoBlu/MapFragment.java
+++ MilanoBlu2/src/com/cesena/milanoBlu/MapFragment.java
@@ -1,20 +1,24 @@
 package com.cesena.milanoBlu;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.cesenaTeam.milanoBlu.R;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends SupportMapFragment {
-
-	GoogleMap mapView;
+public class MapFragment extends Fragment {
 
 	private final static LatLng COORD_MILANO = new LatLng(45.535901f - 0.06f,
 			9.082642f + 0.08f);
@@ -23,58 +27,53 @@ public class MapFragment extends SupportMapFragment {
 	// LatLngBounds MILANO = new LatLngBounds(
 	// new LatLng(45.535901f, 9.082642f), new LatLng(45.412879f, 9.266320f));
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-
-		// inizializzo la mappa
-		mapView = getMap();
-		
-//		addRandomMarkers();
-//		centerCity(COORD_MILANO);
-	}
+	MapView mapView;
+	GoogleMap map;
 
 	@Override
-	public void onViewStateRestored(Bundle savedInstanceState) {
-		super.onViewStateRestored(savedInstanceState);
-       //  mMap.setOnMyLocationButtonClickListener(this);
-		
-		mapView.setMyLocationEnabled(true);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.map_fragment, container, false);
+
+		// Gets the MapView from the XML layout and creates it
+		mapView = (MapView) v.findViewById(R.id.mapview);
+		mapView.onCreate(savedInstanceState);
+
+		// Gets to GoogleMap from the MapView and does initialization stuff
+		map = mapView.getMap();
+		map.getUiSettings().setMyLocationButtonEnabled(false);
+		map.setMyLocationEnabled(true);
+
+		MapsInitializer.initialize(this.getActivity());
 
 		addRandomMarkers();
 		centerCity(COORD_MILANO);
 
+		return v;
 	}
-//
-//	@Override
-//	public void onStart() {
-//super.onStart();
-//		addRandomMarkers();
-//		centerCity(COORD_MILANO);
-//	}
-//
+
 	@Override
 	public void onResume() {
+		mapView.onResume();
 		super.onResume();
-		// personalizzazioni
-//		addRandomMarkers();
-//		centerCity(COORD_MILANO);
-
 	}
 
-	//
-	// @Override
-	// public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	// Bundle savedInstanceState) {
-	// View view = inflater.inflate(R.layout.mapfragments, container, false);
-	// return view;
-	// }
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mapView.onDestroy();
+	}
+
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		mapView.onLowMemory();
+	}
 
 	private void centerCity(LatLng coordinate) {
 		// quanto zoomare sulla città
 		int zoom = 11;
-		mapView.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinate,
-				zoom));
+		map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinate, zoom));
 	}
 
 	private void addMarker(double lat, double lng) {
@@ -83,10 +82,10 @@ public class MapFragment extends SupportMapFragment {
 		MarkerOptions markerOptions = new MarkerOptions();
 
 		markerOptions.position(new LatLng(lat, lng));
-		 markerOptions.icon(BitmapDescriptorFactory.defaultMarker());
-		 markerOptions.icon(BitmapDescriptorFactory
-		 .fromResource(R.drawable.goccia));
-		mapView.addMarker(markerOptions);
+		markerOptions.icon(BitmapDescriptorFactory.defaultMarker());
+		markerOptions.icon(BitmapDescriptorFactory
+				.fromResource(R.drawable.goccia));
+		map.addMarker(markerOptions);
 	}
 
 	private void addRandomMarkers() {
